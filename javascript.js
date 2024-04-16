bb0bd811c3ea22d28df5d3221e08d0de06489028
aliwdg16 <alialtoto@hotmail.com>
2024-04-16 16:42:59 +0200

    edit+localstorge+clearbtn+editbtn

--- javascript.js
+++ javascript.js
@@ -1,5 +1,6 @@
-const aufgabenListe = document.getElementById("item-list");
+const aufgabenListe = document.getElementById("itemlist");
 const textFeld = document.getElementById("add");
+
 // Add Button
 const addButton = document.getElementById("add-btn");
 
@@ -16,6 +17,7 @@ function addItem() {
     li.appendChild(cross);
   }
   textFeld.value = "";
+  Local()
 }
 
 addButton.addEventListener("click", addItem);
@@ -28,19 +30,84 @@ function clearList() {
     alert("Ist doch leer Mensch...");
   } else {
     aufgabenListe.innerHTML = "";
+   
   }
+  Local()
 }
 
 clearButton.addEventListener("click", clearList);
 
 //delete
-const Deletebutton = document.getElementById("item-list");
+const Deletebutton = document.getElementById("itemlist");
 Deletebutton.addEventListener(
   "click",
   function (Deletethis) {
     if (Deletethis.target.matches("cross")) {
       Deletethis.target.parentElement.remove();
+     
     }
+    Local()
   },
   false
 );
+
+
+// Edit button
+
+const editButton = document.getElementById("Edit-button");
+
+function editTask() {
+  const selectedTask = document.querySelector(".list-group-item.selected");
+  if (selectedTask) {
+    const newText = prompt("Edit task:", selectedTask.textContent);
+    if (newText !== null) {
+      selectedTask.textContent = newText;
+    
+    }
+  } else {
+    alert("Please select a task to edit.");
+  }
+  Local()
+}
+
+editButton.addEventListener("click", editTask);
+
+aufgabenListe.addEventListener("click", function (event) {
+  const selectedTask = event.target.closest("li");
+  if (selectedTask) {
+    const isActive = selectedTask.classList.contains("selected");
+   
+    if (isActive) {
+      selectedTask.classList.remove("selected");
+    } else {
+      
+      const tasks = aufgabenListe.querySelectorAll("li");
+      tasks.forEach((task) => task.classList.remove("selected"));
+    
+      selectedTask.classList.add("selected");
+    }
+  }
+});
+
+// LocalStorage
+function Local(){
+  localStorage.setItem("data",itemlist.innerHTML);
+}
+
+function ssh(){
+  itemlist.innerHTML = localStorage.getItem("data");
+}
+ssh();
+
+
+
+
+// //check
+// function check(click) {
+//   const checklist = document.getElementById('item-list');
+//   checklist.addEventListener('click', event=> {
+//     if (event.target.tagName ==='LI') {
+//         event.target.classList.toggle('checkend');
+//     }
+//    });}
+    
